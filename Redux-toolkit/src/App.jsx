a6cdf4774b3ee0ffb02ce20f16316b00395df938
Abhay kumar singh <abhaykumarsingh2904@gmail.com>
2025-01-26 23:18:21 +0530

    add new folder

---
+++ Redux-toolkit/src/App.jsx
@@ -0,0 +1,16 @@
+import AddTodo from "./component/addTodo" 
+
+function App() {
+  
+
+  return (
+    <>
+       <h1 className='bg-amber-500 text-center'>Redux Tool-kit </h1>
+
+       
+       <AddTodo/>
+    </>
+  )
+}
+
+export default App
