a6bce4720457a7a14201840e6e901d00fd168417
Abhay kumar singh <abhaykumarsingh2904@gmail.com>
2025-01-28 22:05:20 +0530

    Creating blog app auth

---
+++ BlogApp/src/App.jsx
@@ -0,0 +1,16 @@
+ 
+import './App.css'
+import Conf from './conf/conf'
+
+function App() {
+ console.log(Conf.appwriteCollectionId)
+
+  return (
+    <>
+     <h1>Mega blog project
+     </h1>
+    </>
+  )
+}
+
+export default App
